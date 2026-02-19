d33969e6a60e1610084121b459dc3c48844ca464
Tanish Pawar <pawartanish446@gmail.com>
2026-02-20 02:25:27 +0530

    Hand detection implemented

---
+++ hand_tracking.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1)
+mp_draw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
+
+            landmarks = []
+            for lm in handLms.landmark:
+                landmarks.append(lm.x)
+                landmarks.append(lm.y)
+
+            print(len(landmarks))  # Should print 42
+
+    cv2.imshow("Hand Tracking", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
